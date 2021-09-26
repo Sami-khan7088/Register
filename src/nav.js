52cad11dd9143e8430eb607ce1b402dda26a8110
Sami-khan7088 <89895044+Sami-khan7088@users.noreply.github.com>
2021-09-26 15:48:35 +0530

    update

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,16 @@
 function Nav() {
     return <>
         <div className="Nav_main">
+        <div className="dropdown">
+            <div className="icon"><i  className="zmdi zmdi-menu"></i></div>
+            <div className="nav">
+            <div className="divi">Home</div>
+            <div className="divi"><a href="index.html" className="href">Registration</a></div>
+            <div className="divi">Login</div>
+            <div className="divi">About Us</div>
+            <div className="divi">Contact</div>
+            </div>
+        </div>
             <div className="nav_logo">
                 <img src="https://secureservercdn.net/160.153.137.40/q6q.af3.myftpupload.com/wp-content/uploads/2019/12/0963-1.png" alt="some" className="logo"/>
             </div>
@@ -12,8 +22,8 @@ function Nav() {
             <div className="divi">Contact</div>
             </div>
         </div>
-        </>
-
+        
+</>
     
 }
 export default Nav;
