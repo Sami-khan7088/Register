336231bf683a1a77b6b6b88206cdfa311c677814
Sami-khan7088 <89895044+Sami-khan7088@users.noreply.github.com>
2021-09-26 09:38:39 +0530

    upload

---
+++ src/nav.js
@@ -0,0 +1,19 @@
+function Nav() {
+    return <>
+        <div className="Nav_main">
+            <div className="nav_logo">
+                <img src="https://secureservercdn.net/160.153.137.40/q6q.af3.myftpupload.com/wp-content/uploads/2019/12/0963-1.png" alt="some" className="logo"/>
+            </div>
+            <div className="nav_items">
+            <div className="divi">Home</div>
+            <div className="divi"><a href="index.html" className="href">Registration</a></div>
+            <div className="divi">Login</div>
+            <div className="divi">About Us</div>
+            <div className="divi">Contact</div>
+            </div>
+        </div>
+        </>
+
+    
+}
+export default Nav;
